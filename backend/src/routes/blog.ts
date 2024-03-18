4ead8befcff6be552d38b1aa785aed5327084a1d
Aurojyoti <developeratbackend@gmail.com>
2024-03-18 23:14:56 +0530

    Encrypting the JWT_SECRET

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -17,12 +17,19 @@ export const blogRouter = new Hono<{
 
 blogRouter.use("/*", async (c, next) => {
   const authHeader = c.req.header("authorization") || "";
-  const user = await verify(authHeader, c.env.JWT_SECRET);
 
-  if (user) {
-    c.set("userId", user.id);
-    await next();
-  } else {
+  try {
+    const user = await verify(authHeader, c.env.JWT_SECRET);
+    if (user) {
+      c.set("userId", user.id);
+      await next();
+    } else {
+      c.status(403);
+      return c.json({
+        message: "you are not logged in",
+      });
+    }
+  } catch (e) {
     c.status(403);
     return c.json({
       message: "you are not logged in",
